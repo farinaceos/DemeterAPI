896093613e2415e83510824f3912d27c49d9bbdc
farinaceos <farinamatheus07@gmail.com>
2021-08-27 10:20:28 -0300

    Criação do model Tema para API

---
+++ demeterAPI/src/main/java/org/generation/demeterAPI/model/Tema.java
@@ -0,0 +1,85 @@
+
+/*MODEL TEMA PARA API DEMETER - Grupo 06
+ *
+ *		Projeto de Rede Social para integrar pessoas com necessidades a possíveis doadores
+ *
+ *
+ *Grupo:
+ *	Anderson
+ * 	Cássia
+ *	Matheus Farina
+ *	Renata
+ *	Vinicius Cardoso
+ */
+package org.generation.demeterAPI.model;
+
+//Pacotes importados
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+//Criação da Tabela
+
+@Entity // -> Define Entidade
+@Table(name = "tb_tema") // -> Dá o nome à tabela no MySQL
+public class Tema {
+
+	//Criação do atributo ID como Primary Key e Auto Increment
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	//Criação do atributo Categoria como String, tamanho máximo de 255 caracteres
+	@NotNull
+	@Size(max = 255)
+	private String categoria;
+	
+	//Criação do atributo Filtro como String, tamanho máximo de 255 caracteres
+	@NotNull
+	@Size(max = 255)
+	private String filtro;
+	
+	//Criação do atributo Localidade como String, tamanho máximo de 255 caracteres
+	@NotNull
+	@Size(max = 255)
+	private String localidade;
+
+	// Início dos Getters e Setters
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getFiltro() {
+		return filtro;
+	}
+
+	public void setFiltro(String filtro) {
+		this.filtro = filtro;
+	}
+
+	public String getLocalidade() {
+		return localidade;
+	}
+
+	public void setLocalidade(String localidade) {
+		this.localidade = localidade;
+	}
+}
