810c5300d70f89b69ae66e72accfc496fc6b1b8f
Reborsolli <renata.ferreirapn@gmail.com>
2021-08-27 15:14:01 -0300

    1

--- demeterAPI/src/main/java/org/generation/demeterAPI/DemeterApiApplication.java
+++ demeterAPI/src/main/java/org/generation/demeterAPI/DemeterApiApplication.java
@@ -1,1 +1,13 @@
+package org.generation.demeterAPI;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemeterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemeterApiApplication.class, args);
+    }
+
+}
