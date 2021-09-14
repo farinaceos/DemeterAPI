7c07756504e91d73483880218c7d5ebd1d5c6195
farinaceos <farinamatheus07@gmail.com>
2021-09-14 10:44:41 -0300

    Alteração de arquivos para Deploy em nuvem

--- demeterAPI/src/main/java/org/generation/demeterAPI/DemeterApiApplication.java
+++ demeterAPI/src/main/java/org/generation/demeterAPI/DemeterApiApplication.java
@@ -2,12 +2,21 @@ package org.generation.demeterAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class DemeterApiApplication {
+	@GetMapping
+	public ModelAndView swaggerUi() {
+		return new ModelAndView("redirect:/swagger-ui/");
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemeterApiApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(DemeterApiApplication.class, args);
+	}
 }
--- demeterAPI/src/main/java/org/generation/demeterAPI/seguranca/BasicSecurityConfig.java
+++ demeterAPI/src/main/java/org/generation/demeterAPI/seguranca/BasicSecurityConfig.java
@@ -2,6 +2,7 @@ package org.generation.demeterAPI.seguranca;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,30 +13,35 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
-public class BasicSecurityConfig extends WebSecurityConfigurerAdapter{
-	
+public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService);
+		auth.inMemoryAuthentication()
+		.withUser("root")
+		.password(passwordEncoder().encode("root"))
+		.authorities("ROLE_USER");
 	}
-	
+
 	@Bean
-	public PasswordEncoder passwordEncoder () {
+	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
+
 	@Override
-	protected void configure(HttpSecurity http) throws Exception	 {
+	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers("/usuario/logar").permitAll()
 		.antMatchers("/usuario/cadastrar").permitAll()
+		.antMatchers(HttpMethod.OPTIONS).permitAll()
 		.anyRequest().authenticated()
 		.and().httpBasic()
-		.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and().sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and().cors()
 		.and().csrf().disable();
 	}
